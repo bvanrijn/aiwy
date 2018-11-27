eefef5186e072e0e265a28382efcd69f4eec0c75
Barend <bvanrijn@users.noreply.github.com>
2018-11-27 16:06:30 +0100

    Upgrade requests dependency

--- extensions/canonical/README.md
+++ extensions/canonical/README.md
@@ -8,13 +8,13 @@ A public instance is running at https://canonical.now.sh.
 Start the server:
 
 ```bash
-$ npm run start
+npm run start
 ```
 
 And then:
 
 ```bash
-$ curl -d '{"url": "http://amp.example.com"}' localhost:443
+curl -d '{"url": "http://amp.example.com"}' localhost:443
 ```
 
 and you get back:
@@ -25,4 +25,19 @@ and you get back:
     "original": "http://amp.example.com",
     "canonical": "http://example.com"
 }
+```
+
+## Your privacy
+
+The extensions in this directory (and the canonical server) are designed to use as little personal data as possible. For instance, the extensions only contact canonical.now.sh over a secure HTTPS connection, and logs are extremely limited in scope:
+
+```
+[01/Sep/2018:23:11:38 +0000] POST / 200 647.165 ms - 201
+```
+
+The canonical server will only produce a detailed log when explicitly asked to do so (using `{"dev": true}`):
+
+```
+[200] (https://www.google.com/amp/s/phys.org/news/2018-08-google-news-liberals-similar-results.amp => https://phys.org/news/2018-08-google-news-liberals-similar-results.html) OK.
+[01/Sep/2018:23:15:33 +0000] POST / 200 754.085 ms - 201
 ```
--- extensions/canonical/index.js
+++ extensions/canonical/index.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio')
 const pify = require('pify')
 const morgan = require('morgan')
 
-const logger = pify(morgan('[:date[clf]] :method :url :status :response-time ms - :res[content-length]')) // Like morgan('dev') but with timestamp
+const logger = pify(morgan(':method :url :status :response-time ms - :res[content-length]'))
 
 module.exports = async (req, res) => {
   await logger(req, res)
@@ -12,14 +12,11 @@ module.exports = async (req, res) => {
   const data = await json(req)
   let response
 
-  const developerMode = data.dev
   const originalURL = data.url
 
-  const log = (devMessage, noDevMessage) => {
-    if (developerMode === true) {
-      console.log(devMessage)
-    } else {
-      console.log(noDevMessage)
+  const devLog = (msg) => {
+    if (data.dev === true) { // developer mode on
+      console.log(msg)
     }
   }
 
@@ -31,15 +28,15 @@ module.exports = async (req, res) => {
 
       const canonicalURL = $('link[rel="canonical"]').attr('href')
 
-      log(`[200] (${originalURL} => ${canonicalURL}) OK.`, '[200] OK.')
+      devLog(`[200] (${originalURL} => ${canonicalURL}) OK.`)
 
       send(res, 200, { ok: true, original: originalURL, canonical: canonicalURL })
     } else {
-      log(`[404] (${originalURL}) Status not OK or Content-Type not 'text/html'.`, "[404] Status not OK or Content-Type not 'text/html'.")
+      devLog(`[404] (${originalURL}) Status not OK or Content-Type not 'text/html'.`)
       send(res, 404, { ok: false, original: originalURL, canonical: null })
     }
   } catch (error) {
-    log(`[500] (${originalURL}) Error while getting the page.`, '[500] Error while getting the page.')
+    devLog(`[500] (${originalURL}) Error while getting the page.`)
     send(res, 500, { ok: false, original: originalURL, canonical: null })
   }
 }
